2d7f79187b607e7821216efa24c29a5e2653ae97
fa-repo <fa.repo.dev@gmail.com>
2019-11-27 14:13:58 +0000

    chore: remove unused laravel api routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -13,34 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-// This endpoint does not need authentication.
-Route::get('/public', function (Request $request) {
-  return response()->json(['message' => 'Hello from a public endpoint!']);
-});
-
-// Article routes
 Route::get('/articles', 'Api\ArticleController@index');
 Route::get('/articles/{article}', 'Api\ArticleController@show');
 Route::middleware(['jwt'])->group(function () {
   Route::post('/articles', 'Api\ArticleController@store');
   Route::patch('/articles/{article}', 'Api\ArticleController@update');
   Route::delete('/articles/{article}', 'Api\ArticleController@destroy');
-});
-
-// These endpoints require a valid access token.
-Route::middleware(['jwt'])->group(function () {
-  Route::get('/private', function (Request $request) {
-      return response()->json(['message' => 'Hello from a private endpoint!']);
-  });
-});
-
-// These endpoints require a valid access token with a "read:messages" scope.
-Route::middleware(['jwt:read:messages'])->group(function () {
-  Route::get('/private-scoped', function (Request $request) {
-      return response()->json(['message' => 'Hello from a private, scoped endpoint!']);
-  });
 });
