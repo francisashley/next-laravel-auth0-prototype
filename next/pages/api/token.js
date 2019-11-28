78c6f3ef653c38933e399d68c23e6bf0a78b303e
fa-repo <fa.repo.dev@gmail.com>
2019-11-28 10:53:16 +0000

    fix: no accessToken crashing /token when logged out

--- next/pages/api/token.js
+++ next/pages/api/token.js
@@ -2,8 +2,9 @@ import auth0 from "../../lib/auth0";
 import nookies from "nookies";
 
 export default async function me(req, res) {
-  const { accessToken, ...a } = await auth0.getSession(req);
-  let managerToken = nookies.get({ req, res }).manager_access_token;
+  const session = await auth0.getSession(req);
+  const accessToken = session ? session.accessToken : "";
+  const explorerToken = nookies.get({ req, res }).manager_access_token || "";
 
-  return res.json({ accessToken, managerToken });
+  return res.json({ accessToken, explorerToken });
 }
