1c8e8a243b80be0851d9a19f061a6eca1d84be8b
fa-repo <fa.repo.dev@gmail.com>
2019-12-02 17:12:59 +0000

    feat: update users table schema for caching auth0 users

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('auth0_id');
+            $table->string('username');
+            $table->string('picture');
             $table->timestamps();
         });
     }
