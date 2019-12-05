2e5c0e099888a559f816af18541ae50901d14d6e
fa-repo <fa.repo.dev@gmail.com>
2019-12-05 13:10:07 +0000

    refactor: move responsibility of getting session user from withUser to withAuth HOC. withAuth also now uses IOC to authorize user.

--- next/lib/withAuth.js
+++ next/lib/withAuth.js
@@ -2,39 +2,42 @@ import React, { Component } from "react";
 import auth0 from "../lib/auth0";
 import { fetchUser } from "../lib/user";
 
-export default function withAuth(AuthComponent) {
-  return class WithAuth extends Component {
-    static async getInitialProps(ctx) {
-      let pageProps = {};
+export default function withAuth(AuthComponent, authorize) {
+    return class WithAuth extends Component {
+        static async getInitialProps(ctx) {
+            let pageProps = {};
 
-      // Check if `user` logged in
-      let user = null;
-      if (typeof window === "undefined") {
-        const session = await auth0.getSession(ctx.req);
-        user = session && session.user;
-      } else {
-        const cookie = ctx.req && ctx.req.headers.cookie;
-        user = await fetchUser(cookie);
-      }
+            // Check if `user` logged in
+            let user = null;
+            if (typeof window === "undefined") {
+                const session = await auth0.getSession(ctx.req);
+                user = session && session.user;
+            } else {
+                const cookie = ctx.req && ctx.req.headers.cookie;
+                user = await fetchUser(cookie);
+            }
 
-      // Redirect if user not found
-      if (!user && typeof window === "undefined") {
-        ctx.res.writeHead(302, { Location: "/api/login" });
-        ctx.res.end();
-        return;
-      } else if (!user && window !== "undefined") {
-        return (window.location.href = "/api/login");
-      }
+            // Authorize externally with IOC (Inversion of Control)
+            const authorized = authorize ? authorize({ user }) : true;
 
-      if (AuthComponent.getInitialProps) {
-        pageProps = await AuthComponent.getInitialProps(ctx);
-      }
+            // Redirect if user not found
+            if (!authorized && typeof window === "undefined") {
+                ctx.res.writeHead(302, { Location: "/api/login" });
+                ctx.res.end();
+                return;
+            } else if (!authorized && window !== "undefined") {
+                return (window.location.href = "/api/login");
+            }
 
-      return { ...pageProps };
-    }
+            if (AuthComponent.getInitialProps) {
+                pageProps = await AuthComponent.getInitialProps(ctx);
+            }
 
-    render() {
-      return <AuthComponent {...this.props} />;
-    }
-  };
+            return { ...pageProps, user };
+        }
+
+        render() {
+            return <AuthComponent {...this.props} />;
+        }
+    };
 }
